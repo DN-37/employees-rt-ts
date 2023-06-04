d25aef115182b705c67897250758fdcf0a014949
Danila Nefedov <dn375@mail.ru>
2023-06-04 20:52:26 +0400

    added links for register and login in header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,10 +1,13 @@
 import {
-    TeamOutlined
+    TeamOutlined,
+    LoginOutlined,
+    UserOutlined,
 } from "@ant-design/icons";
 import { Layout, Space, Typography } from "antd";
 import { Link } from "react-router-dom";
 import { CustomButton } from "../custom-button";
 import style from "./index.module.css";
+import { Paths } from "../../paths";
 
 export const Header = () => {
 
@@ -12,9 +15,21 @@ export const Header = () => {
         <Layout.Header className={style.header}>
             <Space>
                 <TeamOutlined className={style.teamIcon} />
-                <Link to="/">
+                <Link to={Paths.home}>
                     <CustomButton type="ghost">
                         <Typography.Title level={1}>Сотрудники</Typography.Title>
+                    </CustomButton>
+                </Link>
+            </Space>
+            <Space>
+                <Link to={Paths.register}>
+                    <CustomButton type="ghost" icon={<UserOutlined />}>
+                        Зарегистрироваться
+                    </CustomButton>
+                </Link>
+                <Link to={Paths.login}>
+                    <CustomButton type="ghost" icon={<LoginOutlined />}>
+                        Войти
                     </CustomButton>
                 </Link>
             </Space>
