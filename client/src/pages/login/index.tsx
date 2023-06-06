50208a89c5e9ef58ab8276bf02e476afab6dae42
Danila Nefedov <dn375@mail.ru>
2023-06-06 18:40:56 +0400

    created custom-input

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,10 +1,18 @@
 import { Layout } from "../../components/layout";
 import React from 'react';
+import { Card, Form, Row } from "antd";
+import { CustomInput } from "../../components/custom-input";
 
 export const Login = () => {
     return (
         <Layout>
-            <div>Login</div>
+            <Row align="middle" justify="center">
+                <Card title="Войдите" style={{ width: "30rem" }}>
+                    <Form onFinish={() => null}>
+                        <CustomInput type="email" name="email" placeholder="Email" />
+                    </Form>
+                </Card>
+            </Row>
         </Layout>
     )
 }
