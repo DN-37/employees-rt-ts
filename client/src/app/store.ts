a0d06ed1efc531de69f695fe398bb9f93b04a1dd
Danila Nefedov <dn375@mail.ru>
2023-06-17 10:52:35 +0400

    created api for employees

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -2,11 +2,13 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { api } from "./services/api";
 import auth from '../features/auth/authSlice';
 import { listenerMiddleware } from "../middleware/auth";
+import employees from '../features/employees/employeesSlice'
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth,
+    employees,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware).prepend(listenerMiddleware.middleware),
 });
