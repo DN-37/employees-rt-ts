c04103f47c59e909b809f566bb4928cd0f7ab300
Danila Nefedov <dn375@mail.ru>
2023-07-05 21:27:37 +0400

    made base markup for page employee

--- client/src/pages/employee/index.tsx
+++ client/src/pages/employee/index.tsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Descriptions, Space, Divider } from "antd";
+import { CustomButton } from "../../components/custom-button";
+import { useState } from "react";
+import { useNavigate, Link, useParams, Navigate } from "react-router-dom";
+import {
+    useGetEmployeeQuery,
+    useRemoveEmployeeMutation,
+} from "../../app/services/employees";
+import { Layout } from "../../components/layout";
+import { ErrorMessage } from "../../components/error-message";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/auth/authSlice";
 
 export const Employee = () => {
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const params = useParams<{ id: string }>();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { data, isLoading } = useGetEmployeeQuery(params.id || "");
+    const user = useSelector(selectUser);
+
+    if (isLoading) {
+        return <span>Загрузка</span>;
+    }
+
+    if (!data) {
+        return <Navigate to="/" />;
+    }
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+
     return (
-        <div>Employee</div>
-    )
-}
+        <Layout>
+            <Descriptions title="Информация о сотруднике" bordered>
+                <Descriptions.Item
+                    label="Имя"
+                    span={3}
+                >{`${data.firstName} ${data.lastName}`}</Descriptions.Item>
+                <Descriptions.Item label="Возраст" span={3}>
+                    {data.age}
+                </Descriptions.Item>
+                <Descriptions.Item label="Адрес" span={3}>
+                    {data.address}
+                </Descriptions.Item>
+            </Descriptions>
+            {user?.id === data.userId && (
+                <>
+                    <Divider orientation="left">Действия</Divider>
+                    <Space>
+                        <Link to={`/employee/edit/${data.id}`}>
+                            <CustomButton
+                                shape="round"
+                                type="default"
+                                icon={<EditOutlined />}
+                            >
+                                Редактировать
+                            </CustomButton>
+                        </Link>
+                        <CustomButton
+                            shape="round"
+                            danger
+                            onClick={showModal}
+                            icon={<DeleteOutlined />}
+                        >
+                            Удалить
+                        </CustomButton>
+                    </Space>
+                </>
+            )}
+            <ErrorMessage message={error} />
+        </Layout>
+    );
+};
