f44d7146e0e66dec92f80f9d66214b5e67c8e138
Danila Nefedov <dn375@mail.ru>
2023-07-07 22:19:44 +0400

    added modal for page employee

--- client/src/pages/employee/index.tsx
+++ client/src/pages/employee/index.tsx
@@ -1,13 +1,15 @@
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-import { Descriptions, Space, Divider } from "antd";
+import { Descriptions, Space, Divider, Modal } from "antd";
 import { CustomButton } from "../../components/custom-button";
 import { useState } from "react";
+import { Paths } from "../../paths";
 import { useNavigate, Link, useParams, Navigate } from "react-router-dom";
 import {
     useGetEmployeeQuery,
     useRemoveEmployeeMutation,
 } from "../../app/services/employees";
 import { Layout } from "../../components/layout";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
 import { ErrorMessage } from "../../components/error-message";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/auth/authSlice";
@@ -18,6 +20,7 @@ export const Employee = () => {
     const params = useParams<{ id: string }>();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const { data, isLoading } = useGetEmployeeQuery(params.id || "");
+    const [removeEmployee] = useRemoveEmployeeMutation();
     const user = useSelector(selectUser);
 
     if (isLoading) {
@@ -32,6 +35,27 @@ export const Employee = () => {
         setIsModalOpen(true);
     };
 
+    const hideModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleDeleteUser = async () => {
+        hideModal();
+
+        try {
+            await removeEmployee(data.id).unwrap();
+
+            navigate(`${Paths.status}/deleted`);
+        } catch (err) {
+            const maybeError = isErrorWithMessage(err);
+
+            if (maybeError) {
+                setError(err.data.message);
+            } else {
+                setError("Неизвестная ошибка");
+            }
+        }
+    };
 
     return (
         <Layout>
@@ -72,6 +96,16 @@ export const Employee = () => {
                 </>
             )}
             <ErrorMessage message={error} />
+            <Modal
+                title="Подтвердите удаление"
+                open={isModalOpen}
+                onOk={handleDeleteUser}
+                onCancel={hideModal}
+                okText="Подтвердить"
+                cancelText="Отменить"
+            >
+                Вы действительно хотите удалить сотрудника из таблицы?
+            </Modal>
         </Layout>
     );
 };
