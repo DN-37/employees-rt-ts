db5d2281cfbd83b2697f9fddcbd4ecd690d7cb99
Danila Nefedov <dn375@mail.ru>
2023-08-05 11:51:11 +0400

    made store

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,16 +1,19 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 import { api } from "./services/api";
-import auth from '../features/auth/authSlice';
-import { listenerMiddleware } from "../middleware/auth";
+import auth from '../features/auth/authSlice'
 import employees from '../features/employees/employeesSlice'
+import { listenerMiddleware } from "../middleware/auth";
 
 export const store = configureStore({
   reducer: {
+    counter: counterReducer,
     [api.reducerPath]: api.reducer,
     auth,
-    employees,
+    employees
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware).prepend(listenerMiddleware.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware).prepend(listenerMiddleware.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
